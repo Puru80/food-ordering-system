3f3eda2d2d677c29a4ff51f39b937087d87484a1
puru-flash <puruagarwal@flash.tech>
2023-07-23 16:42:08 +0530

    Initialized git repo

---
+++ src/main/java/com/bros/fooforderingsystem/FoofOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bros.fooforderingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoofOrderingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoofOrderingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bros/fooforderingsystem/FoofOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bros.fooforderingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoofOrderingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
