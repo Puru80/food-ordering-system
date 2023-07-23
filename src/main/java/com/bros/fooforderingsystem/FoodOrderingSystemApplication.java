1db54f968ca950becdd030dd141f8f4c33379dc4
Puru80 <puru.agar99@gmail.com>
2023-07-23 17:15:42 +0530

    refactored ackage naming

--- src/main/java/com/bros/fooforderingsystem/FoodOrderingSystemApplication.java
+++ src/main/java/com/bros/fooforderingsystem/FoodOrderingSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FoofOrderingSystemApplication {
+public class FoodOrderingSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FoofOrderingSystemApplication.class, args);
+        SpringApplication.run(FoodOrderingSystemApplication.class, args);
     }
 
 }
--- src/test/java/com/bros/fooforderingsystem/FoodOrderingSystemApplicationTests.java
+++ src/test/java/com/bros/fooforderingsystem/FoodOrderingSystemApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FoofOrderingSystemApplicationTests {
+class FoodOrderingSystemApplicationTests {
 
     @Test
     void contextLoads() {
